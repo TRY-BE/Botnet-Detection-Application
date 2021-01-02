c2af411b14a0f0f8c855974d245422db2c31935f
Yash Patwa <yashpatwa44@gmail.com>
2021-01-02 21:25:04 +0530

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+/* jshint ignore:start */
+import React from "react";
+import ReactDOM from "react-dom";
 
-
+ReactDOM.render(
+    <div>
+        <h1>Hello World!</h1>
+        <p>This is my final year project</p>
+    </div>,
+    document.getElementById("root")
+);
+/* jshint ignore:end */
